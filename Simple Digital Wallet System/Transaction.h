014dfffe5df4ad8db1822abc38a1cfa202c55971
Mohamed Ali <115904269+mohamedali382@users.noreply.github.com>
2024-04-15 01:40:12 +0200

    Update Transaction.h

--- Simple Digital Wallet System/Transaction.h
+++ Simple Digital Wallet System/Transaction.h
@@ -1,7 +1,26 @@
-#pragma once
+#include <iostream>
+#include<ctime>
+#include <chrono>
+using namespace std;
+
+struct DATE {
+	int hour,min, day, month, year;
+};
 
 class Transaction
 {
+	int transactionID;
+	string sender, reciever;
+	float amount;
+	DATE date;
 
+public:
+	 Transaction(string sender,string reciever, float amount,DATE date);
+
+	 void fillData(string reciever, float amount);
+	 bool CheckBalance();
+	 Transaction CheckOut();
+	 ~Transaction();
+	
 };
 

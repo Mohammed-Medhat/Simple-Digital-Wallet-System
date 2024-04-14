405a4e0c8c1fe8ddee81e4c413a28c0419bc0300
Ali-Madian <167026819+Ali-Madian@users.noreply.github.com>
2024-04-14 21:48:17 +0200

    Admin.h

--- Simple Digital Wallet System/Admin.h
+++ Simple Digital Wallet System/Admin.h
@@ -1,5 +1,27 @@
 #pragma once
+#include <string>
+
+using namespace std;
+
 class Admin
 {
+private :
+	string username;
+	string password;
+
+protected :
+	
+	void set_username(string new_name);
+	string get_username();
+
+	void set_password(string new_pass);
+	string get_password();
+	
+	void view_Accounts_Data();
+	void edit_user(string name);
+	void delete_user(string name);
+	void suspend_user(string name);
+	void view_all_transactions();
+	void edit_balance(string name);
 };
 

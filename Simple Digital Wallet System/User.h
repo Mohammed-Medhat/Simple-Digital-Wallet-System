d656510417a6e28c4024e28f7d940f23c8a32c19
Mohamed Ali <115904269+mohamedali382@users.noreply.github.com>
2024-04-15 00:05:37 +0200

    Update User.h

--- Simple Digital Wallet System/User.h
+++ Simple Digital Wallet System/User.h
@@ -1,5 +1,13 @@
 #pragma once
 class User
 {
+	string UserName, Password;
+	float balance;
+
+	double Send();
+	double Request();
+	double ViewCurrentBalance();
+	Transactions ViewHistory();
+
 };
 

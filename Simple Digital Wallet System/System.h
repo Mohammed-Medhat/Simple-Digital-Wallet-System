61961e29fe61cec6e970dc45695e23b41df1336f
Ali-Madian <167026819+Ali-Madian@users.noreply.github.com>
2024-05-09 02:16:38 +0300

    Update System.h

--- Simple Digital Wallet System/System.h
+++ Simple Digital Wallet System/System.h
@@ -11,9 +11,16 @@
 #include "User.h"
 #include"Transaction.h"
 #include"Admin.h"
-
+#include <iostream>
+#include <string>
+#include <cryptopp/sha.h>
+#include <cryptopp/hex.h>
+#include<assert.h>
+#include <cassert>
+#include <conio.h>
 
 using namespace std;
+
 class User;
 
 class System
@@ -30,7 +37,7 @@ public:
 	static void removeUser(string&);
 	static User* getUser(string username);
 	static bool Login();
-	static bool Register();
+	static bool Register(string,string);
 	static void Logout();
 	static void showAllUser();
 	static bool search_user(string);
@@ -40,8 +47,8 @@ public:
 	static void readPendingRequests();
 	static void readUsersFromFile();
 
-	string md5(const string& input);
-	string SecureString();
+	static string sha256( string& input);
+	static string SecureString();
 
 	static User* getUserForTrans(string username);
 	static void writeUsersToFile();
